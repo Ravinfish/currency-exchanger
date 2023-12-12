0525cc88587f394421aeec8624712fc95923424e
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-12 08:52:00 -0800

    Starting to debug

--- src/js/ConvertCurrency.js
+++ src/js/ConvertCurrency.js
@@ -3,15 +3,14 @@ export default class ConvertCurrency {
   static async convertCurrency(cur1, cur2, amount) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${cur1}/${cur2}/${amount}`);
-      const resultText = await response.text();
-      const result = JSON.parse(resultText);
+      const resultText = await response.json();
 
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText} ${resultText["error-type"]}`;
         throw new Error(errorMessage);
       }
 
-      return { result }
+      return { resultText }
     } catch (error) {
       return { error };
     }
