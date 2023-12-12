583bb1df90a3953e56ccd23f916ba8ae106e48d0
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-11 20:56:32 -0800

    still not functioning. reworking code to make more sense

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import ConvertCurrency from './js/Convert';
+import ConvertCurrency from './js/ConvertCurrency';
+import "./css/styles.css"
 
 document.addEventListener('DOMContentLoaded', () => {
   const exchangeForm = document.getElementById('exchangeForm');
-  const resultsDiv = document.getElementById('results');
+  const resultDiv = document.getElementById('result');
 
   exchangeForm.addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -14,13 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const result = await ConvertCurrency.convertCurrency(cur1, cur2, amount);
 
     if (result.error) {
-      resultsDiv.innerHTML = `<p>Error: ${result.error}</p>`;
+      resultDiv.innerHTML = `<p>Error: ${result.error}</p>`;
     } else {
-      resultsDiv.innerHTML = `
+      resultDiv.innerHTML = `
         <p>${amount} ${cur1} is equal to ${result.convertedAmount} ${cur2}</p>
         <p>Exchange rate: 1 ${cur1} = ${result.exchangeRate} ${cur2}</p>`;
     }
 
-    resultsDiv.classList.remove('hidden')
+    resultDiv.classList.remove('hidden')
   });
 });
