867444913fef876ca099ef47a6f956ed5ea3daaa
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-11 16:09:56 -0800

    working on UI to make form work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import React from 'react';
+import ConvertCurrency from './js/convert';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const exchangeForm = document.getElementById('exchangeForm');
+  const resultsDiv = document.getElementById('results');
+
+  exchangeForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const amount = parseFloat(document.getElementById('amount').value);
+    const cur1 = document.getElementById('cur1').value;
+    const cur2 = document.getElementById('cur2').value;
+
+    const result = await ConvertCurrency.convertCurrency(cur1, cur2, amount);
+
+    if (results.error) {
+      resultsDiv.innerHTML = `<p>Error: ${result.error}</p>`;
+    } else {
+      resultsDiv.innerHTML = `
+        <p>${amount} ${cur1} is equal to ${result.convertedAmount} ${cur2}</p>
+        <p>Exchange rate: 1 ${cur1} = ${result.exchangeRate} ${cur2}</p>`;
+    }
+
+    resultsDiv.classList.remove('hidden')
+  })
+})
--- src/js/Convert.js
+++ src/js/Convert.js
@@ -1,5 +1,16 @@
-static async convertCurrency(cur1, cur2) {
+export default class ConvertCurrency {
+
+static async convertCurrency(cur1, cur2, amount) {
   try {
     const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${cur1}/${cur2}`);
+    const responseData = await response.json();
+    if (!response.ok) {
+      const errorMessage = `${response.status} ${response.statusText} ${responseData["error-type"]}`;
+      throw new Error(errorMessage);
+    }
+    return responseData;
+  } catch (error) {
+    return error;
+    }
   }
 }
