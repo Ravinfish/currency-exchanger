8d7ea66412aec3b145600adbe179c1a37a38f3c6
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-12-11 22:00:34 -0800

    getting undefined error when submitting

--- src/index.js
+++ src/index.js
@@ -1,27 +1,65 @@
 import ConvertCurrency from './js/ConvertCurrency';
 import "./css/styles.css"
 
-document.addEventListener('DOMContentLoaded', () => {
-  const exchangeForm = document.getElementById('exchangeForm');
-  const resultDiv = document.getElementById('result');
+async function handleExchangeForm(e) {
+  e.preventDefault();
+  const cur1 = document.getElementById("cur1").value;
+  const cur1Name = cur1.options[cur1.selectedIndex].text;
+  const cur2 = document.getElementById("cur2").value;
+  const cur2Name = cur2.options[cur2.selectedIndex].text;
+  const amount = document.getElementById("amount").value;
+  const { result, error } = await ConvertCurrency(cur1, cur2, amount);
+  if (error) {
+    const errorMessage = {
+      message: 'An error occured during currency conversion.',
+      errorType: error.message,
+    };
+    const displayError = document.createElement("p");
+    displayError.append(errorMessage.message + "." + errorMessage.errorType + ".");
+    const result = document.getElementById("result");
+    result.innerHTML = "";
+    result.removeAttribute("class");
+    result.append(displayError);
+  } else if (typeof result.conversion_result !== 'undefined') {
+    const conversion = document.createElement("p");
+    conversion.innerHTML = `${amount} ${cur1Name} is equal to ${result.conversion_result} ${cur2Name}`;
+    const result = document.getElementById("result");
+    result.innerHTML = "";
+    result.removeAttribute("class");
+    result.append(conversion);
+  } else {
+    const blank = document.createElement("p");
+    blank.innerHTML = `ERROR. Please input amount.`;
+    const result = document.getElementById("result");
+    result.innerHTML = "";
+    result.removeAttribute("class");
+    result.append(blank);
+  }
+}
 
-  exchangeForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+document.getElementById("convertExchange").addEventListener("submit", handleExchangeForm);
 
-    const amount = parseFloat(document.getElementById('amount').value);
-    const cur1 = document.getElementById('cur1').value;
-    const cur2 = document.getElementById('cur2').value;
+// document.addEventListener('DOMContentLoaded', () => {
+//   const exchangeForm = document.getElementById('exchangeForm');
+//   const resultDiv = document.getElementById('result');
 
-    const result = await ConvertCurrency.convertCurrency(cur1, cur2, amount);
+//   exchangeForm.addEventListener('submit', async (event) => {
+//     event.preventDefault();
 
-    if (result.error) {
-      resultDiv.innerHTML = `<p>Error: ${result.error}</p>`;
-    } else {
-      resultDiv.innerHTML = `
-        <p>${amount} ${cur1} is equal to ${result.convertedAmount} ${cur2}</p>
-        <p>Exchange rate: 1 ${cur1} = ${result.exchangeRate} ${cur2}</p>`;
-    }
+//     const amount = parseFloat(document.getElementById('amount').value);
+//     const cur1 = document.getElementById('cur1').value;
+//     const cur2 = document.getElementById('cur2').value;
 
-    resultDiv.classList.remove('hidden')
-  });
-});
+//     const result = await ConvertCurrency.convertCurrency(cur1, cur2, amount);
+
+//     if (result.error) {
+//       resultDiv.innerHTML = `<p>Error: ${result.error}</p>`;
+//     } else {
+//       resultDiv.innerHTML = `
+//         <p>${amount} ${cur1} is equal to ${result.convertedAmount} ${cur2}</p>
+//         <p>Exchange rate: 1 ${cur1} = ${result.exchangeRate} ${cur2}</p>`;
+//     }
+
+//     resultDiv.classList.remove('hidden')
+//   });
+// });
--- src/js/ConvertCurrency.js
+++ src/js/ConvertCurrency.js
@@ -1,22 +1,19 @@
 export default class ConvertCurrency {
 
-static async convertCurrency(cur1, cur2, amount) {
-  try {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${cur1}/${cur2}/${amount}`);
-    const resultText = await response.text();
-    const result = JSON.parse(resultText);
+  static async convertCurrency(cur1, cur2, amount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${cur1}/${cur2}/${amount}`);
+      const resultText = await response.text();
+      const result = JSON.parse(resultText);
 
-    if (!response.ok) {
-      const errorMessage = `${response.status} ${response.statusText} ${resultText["error-type"]}`;
-      throw new Error(errorMessage);
-    }
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${resultText["error-type"]}`;
+        throw new Error(errorMessage);
+      }
 
-    return { result }
-  } catch (error) {
-      return  {
-        error: error.message || 'An error occured during currency conversion.'
-
-      };
+      return { result }
+    } catch (error) {
+      return { error };
     }
   }
 }
